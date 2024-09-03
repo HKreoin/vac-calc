937760668a072537483d4e4d16088dc348ec8048
Kreoin <steamuser85@yandex.ru>
2024-09-03 14:34:50 +0300

    new maven spring java 11 project: Vacation calculator

---
+++ src/main/java/com/neoflex/vac_calc/VacCalcApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.vac_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoflex/vac_calc/VacCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoflex.vac_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
