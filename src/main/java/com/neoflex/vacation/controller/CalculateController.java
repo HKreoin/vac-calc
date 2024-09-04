80288a20a229a38057b6858ce3094deb13b85e2d
Kreoin <steamuser85@yandex.ru>
2024-09-04 14:48:06 +0300

    change port to 8081, add swagger

--- src/main/java/com/neoflex/vacation/controller/CalculateController.java
+++ src/main/java/com/neoflex/vacation/controller/CalculateController.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.neoflex.vacation.utils.Calculator.calculateVacation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Vacation", description = "Сервис для подсчета отпускных")
 public final class CalculateController {
 
     @GetMapping(value = "calculate")
+    @Operation(summary = "Передача данных о средней зарплате и количестве дней отпуска",
+        responses = {@ApiResponse(description = "Сумма отпускных")})
     public Map<String, Double> calculate(@RequestParam(defaultValue = "0") double averageSalary,
                                             @RequestParam(defaultValue = "0") int vacationDays) {
         var vacationPay = calculateVacation(averageSalary, vacationDays);
